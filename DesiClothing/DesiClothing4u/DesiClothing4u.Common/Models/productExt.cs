df98bd88e4f6dbfec92302b92c505d5e6e2bfde8
unknown <mohtashim@rightsolutions4u.com>
2021-08-29 02:37:24 +0500

    commit on 29082021

---
+++ DesiClothing/DesiClothing4u/DesiClothing4u.Common/Models/productExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesiClothing4u.Common.Models
+{
+    public class productExt
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Discount { get; set; }
+        public string SeoFilename { get; set; }
+
+        public string VirtualPath { get; set; }
+
+    }
+}
